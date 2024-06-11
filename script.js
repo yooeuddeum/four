7310fd46bf3ccdfb8609cd33f3324fb0316fe2e9
유으뜸 <www.goolge.com>
2024-06-11 09:00:29 +0900

    폼 컴포넌트

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ const getUserInfo = () => {
   return {
     name: "유으뜸",
     age: 11,
-    job: "수강생"
-  }
-}
+    job: "수강생",
+  };
+};
+
+const formComponent = (action, method) => {
+  const userInfo = getUserInfo();
+  return `
+  <form action = "${acrion}" method= "${method}">
+  <label for = "name">이름:<label>
+  <input type ="text" id = "name" name= "name" value = "${userInfo.name}" required>
+  <label for = "age">나이:<label>
+  <input type ="number" id = "age" name= "age" value = "${userInfo.age}" required>
+  <label for = "job">직업:<label>
+  <input type ="text" id = "job" name= "job" value = "${userInfo.name}" required>
+  <button type = "submit">제출</button>
+  </form>
+  `;
+};
